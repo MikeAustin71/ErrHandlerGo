f930abdd838894dc37cf51bb61f0c5f2b4a667f7
Mike Rapp <mike@paladinacs.net>
2017-05-19 20:30:19 -0500

    Added 'panic' example

--- 01_ErrUtil/app/main.go
+++ 01_ErrUtil/app/main.go
@@ -2,33 +2,20 @@ package main
 
 import (
 	"MikeAustin71/ErrHandlerGo/01_ErrUtil/common"
+)
+
+/*
+
+import (
+	"MikeAustin71/ErrHandlerGo/01_ErrUtil/common"
 	"errors"
 	"fmt"
 )
 
+*/
+
 func main() {
 
-	bi := common.ErrBaseInfo{}
-
-	f := bi.New("TestSourceFileName", "TestFuncName", 9000)
-	g := bi.New("TestSrcFileName2", "TestFuncName2", 14000)
-	h := bi.New("TestSrcFileName3", "TestFuncName3", 15000)
-
-	ex1 := make([]common.ErrBaseInfo, 0, 10)
-	ex1 = append(ex1, f, g, h)
-
-	ex2_1 := "TestSrcFileName99"
-	ex2_2 := "TestFuncName99"
-	ex2_3 := int64(16000)
-	ex2 := bi.New(ex2_1, ex2_2, ex2_3)
-
-	ex3 := "prefixString"
-	ex4 := "Error Msg 99"
-	err := errors.New(ex4)
-	ex6 := int64(22)
-
-	x := common.SpecErr{}.Initialize(ex1, ex2, ex3, err, false, ex6)
-
-	fmt.Println(x.Error())
+	common.TestEx002()
 
 }
---
+++ 01_ErrUtil/common/errutilexamples.go
@@ -0,0 +1,63 @@
+package common
+
+import (
+	"errors"
+	"fmt"
+)
+
+// TestEx001 - Test Example designed to print
+// multiple parent data associated with a
+// single error
+func TestEx001() {
+	bi := ErrBaseInfo{}
+
+	f := bi.New("TestSourceFileName", "TestFuncName", 9000)
+	g := bi.New("TestSrcFileName2", "TestFuncName2", 14000)
+	h := bi.New("TestSrcFileName3", "TestFuncName3", 15000)
+
+	ex1 := make([]ErrBaseInfo, 0, 10)
+	ex1 = append(ex1, f, g, h)
+
+	ex2_1 := "TestSrcFileName99"
+	ex2_2 := "TestFuncName99"
+	ex2_3 := int64(16000)
+	ex2 := bi.New(ex2_1, ex2_2, ex2_3)
+
+	ex3 := "prefixString"
+	ex4 := "Error Msg 99"
+	err := errors.New(ex4)
+	ex6 := int64(22)
+
+	x := SpecErr{}.Initialize(ex1, ex2, ex3, err, false, ex6)
+
+	fmt.Println(x.Error())
+
+}
+
+// TestEx002 -
+func TestEx002() {
+	// Set up Parent Info
+	bi := ErrBaseInfo{}
+
+	f := bi.New("TestSourceFileName", "TestFuncName", 9000)
+	g := bi.New("TestSrcFileName2", "TestFuncName2", 14000)
+	h := bi.New("TestSrcFileName3", "TestFuncName3", 15000)
+
+	ex1 := make([]ErrBaseInfo, 0, 10)
+	ex1 = append(ex1, f, g, h)
+
+	// Set up BaseInfo
+	ex2_1 := "TestSrcFileName99"
+	ex2_2 := "TestFuncName99"
+	ex2_3 := int64(16000)
+	ex2 := bi.New(ex2_1, ex2_2, ex2_3)
+
+	ex3 := "prefixString"
+	ex4 := "Error Msg 99"
+	err := errors.New(ex4)
+	ex6 := int64(22)
+
+	x := SpecErr{}.Initialize(ex1, ex2, ex3, err, true, ex6)
+
+	panic(x)
+}
